d0199fdfa94ff8e7963b2e3b72411df2b6e766f7
lisandro52 <lisandro52@gmail.com>
2025-08-20 22:22:40 -0300

    Changed files to make gh pages work

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 process.env.VUE_APP_VERSION = process.env.npm_package_version
 
 module.exports = {
+  // Set the base path for GitHub Pages deployment
+  publicPath: process.env.NODE_ENV === 'production' ? '/yet-another-wm-selector/' : '/',
+  
   css: {
     loaderOptions: {
       sass: {
